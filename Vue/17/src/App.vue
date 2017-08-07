79fefdc2f5633389f3c88a96cd66e39920613b19
bonfy <foreverbonfy@163.com>
2017-08-07 22:24:37 +0800

    add Vue 17

---
+++ Vue/17/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+
+    <p>{{ greet() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Hello from Vue component'
+    }
+  },
+  methods: {
+    greet: function(){
+      return 'Hello world from vue methods';
+    }
+  }
+}
+</script>
